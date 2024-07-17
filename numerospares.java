ba82580b8ae2c024a28b1513f0fd68560f9d6320
CristhyPineda25 <pinedasoad25@gmail.com>
2024-07-17 06:13:56 +0200

    Add files via upload

---
+++ numerospares.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package primeratarea;
+
+/**
+ *
+ *Escribe un programa que imprima mediante un ciclo los números pares del 2
+al 100 
+ */
+public class numerospares {
+ 
+    public static void main(String[] args) {
+        // Iteramos desde 2 hasta 100 en incrementos de 2
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }  
+}
